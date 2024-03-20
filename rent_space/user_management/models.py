526d53a3a96a8b16e814e95e11b090f6d110264a
Egecan Giray <girayegecan02@gmail.com>
2024-03-20 12:50:04 +0100

    adding methods to user and admin

--- rent_space/user_management/models.py
+++ rent_space/user_management/models.py
@@ -1,9 +1,7 @@
-from django.contrib.auth.models import AbstractUser
 from django.db import models
-from rent_space.space_booking.models import Booking, AdSpace
+from django.contrib.auth.models import AbstractUser
 
 
-# Create your models here.
 class User(AbstractUser):
     contactInfo = models.CharField(max_length=100)
     role = models.CharField(max_length=50)
@@ -11,4 +9,45 @@ class User(AbstractUser):
     myPayments = models.ManyToManyField('Payment', related_name='my_payments')
     ratings = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     spaces = models.ManyToManyField('AdSpace', related_name='user_spaces')
+    bookings = models.ManyToManyField('Booking', related_name='Bookings')
+
     # name, email, password (inherited from AbstractUser)
+
+    def login(self):
+        pass
+
+    def logout(self):
+        pass
+
+    def updateAccount(self):
+        pass
+
+    def deleteAccount(self):
+        pass
+
+    def searchAdSpace(self):
+        pass
+
+    def bookAdSpace(self):
+        pass
+
+    def addAdSpace(self):
+        pass
+
+    def manageSpace(self):
+        pass
+
+
+class AdminUser(User):
+
+    def approveUser(self):
+        pass
+
+    def removeUser(self):
+        pass
+
+    def moderateContent(self):
+        pass
+
+    def generateReports(self):
+        pass
