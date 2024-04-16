9353fdfeb6104db979ac567b4789a6c2364d7884
Egecan Giray <girayegecan02@gmail.com>
2024-04-16 23:57:10 +0200

    Added CRUD for payment

--- rent_space/space_booking/serializers.py
+++ rent_space/space_booking/serializers.py
@@ -1,13 +1,19 @@
-from .models import AdSpace
-
 # Create operation
 from rest_framework import serializers
-from .models import AdSpace, Rating, Booking
+
+from .models import AdSpace
+from .models import AdSpace, Rating, Booking, Payment
 
 
 class AdSpaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = AdSpace
+        fields = '__all__'
+
+
+class PaymentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Payment
         fields = '__all__'
 
 
@@ -17,9 +23,7 @@ class RatingSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-
 class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = ['client', 'adSpace', 'bookingDate', 'status']
-
