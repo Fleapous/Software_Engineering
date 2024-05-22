2335354b1e4d6f1c4516ff29a4cb7c73b7cbaf6f
Egecan Giray <girayegecan02@gmail.com>
2024-05-22 11:46:29 +0200

    made changes to update approving ad space

--- rent_space/space_booking/serializers.py
+++ rent_space/space_booking/serializers.py
@@ -5,17 +5,12 @@ from .models import AdSpace
 from .models import AdSpace, Rating, Booking, Payment
 from user_management.serializers import UserSerializer  # Import the UserSerializer
 
-class NotApprovedAdSpaceSerializer(serializers.ModelSerializer):
-    ad_space_id = serializers.IntegerField(source='id', read_only=True)
-    owner_id = serializers.IntegerField(source='owner.id', read_only=True)
-    username = serializers.CharField(source='owner.username', read_only=True)
-    email = serializers.EmailField(source='owner.email', read_only=True)
-    first_name = serializers.CharField(source='owner.first_name', read_only=True)
-    last_name = serializers.CharField(source='owner.last_name', read_only=True)
 
+
+class NotApprovedAdSpaceSerializer(serializers.ModelSerializer):
     class Meta:
         model = AdSpace
-        fields = ['ad_space_id', 'owner_id', 'username', 'email', 'first_name', 'last_name', 'location', 'size', 'price', 'availability', 'isApproved']
+        fields = ['id', 'location', 'size', 'price', 'owner_id']
 
 
 class AdSpaceSerializer(serializers.ModelSerializer):
